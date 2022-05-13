fca9f65fb3a331583d8c461a29229bb86de7f8a4
Medeiros84 <dfdimedeiros@hotmail.com>
2022-05-13 17:55:19 -0300

    mauricio-projeto-todo-front-final

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Todo } from '../models/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  baseURL = environment.baseUrl;
+
+  constructor(private http: HttpClient, private snack: MatSnackBar) { }
+
+  findAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.baseURL);
+  }
+
+  findById(id: any): Observable<Todo> {
+    const url = `${this.baseURL}/${id}`
+    return this.http.get<Todo>(url);
+  }
+
+  update(todo: Todo): Observable<Todo> {
+    const url = `${this.baseURL}/${todo.id}`
+    return this.http.put<Todo>(url, todo);
+  }
+
+  delete(id: any):Observable<void>{
+    const url = `${this.baseURL}/${id}`
+    return this.http.delete<void>(url);
+  }
+
+  create(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.baseURL, todo);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 4000
+    })
+  }
+
+}
